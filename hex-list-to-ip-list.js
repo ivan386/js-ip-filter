26268ff8f2bf38c2d3b61693c9fab48c88ec9710
ivan386 <ivan386@users.noreply.github.com>
2019-01-18 12:33:40 +0300

    Fix hex converter

--- hex-list-to-ip-list.js
+++ hex-list-to-ip-list.js
@@ -5,7 +5,7 @@ function to_ip_list(hex_list)
 	for (var i = 0; i < hex_list.length; i++)
 	{
 		var ip = parseInt(hex_list[i], 16)
-		ip_list.push([(ip >> 8*3) & 255, (ip >> 8*2) & 255, (ip >> 8) & 255, ip & 255].join("."))
+		ip_list.push([(ip >>> 8*3) & 255, (ip >>> 8*2) & 255, (ip >>> 8) & 255, ip & 255].join("."))
 	}
 	return ip_list
 }
