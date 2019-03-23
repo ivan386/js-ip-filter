7144274e5348ce01922805bd9492007ae104c09f
ivan386 <ivan386@users.noreply.github.com>
2019-03-23 21:19:17 +0300

    Больше информации по фильтру

--- test-ip-filter.js
+++ test-ip-filter.js
@@ -2,6 +2,27 @@
 
 if (typeof(WScript) != "undefined")
 {
+	WScript.StdErr.Write("ip_filter.ip_list: " + ip_filter.ip_list.length + "\n");
+	
+	var len = 0;
+	for (key in  ip_filter.bitset)
+		len += 1;
+	
+	WScript.StdErr.Write("ip_filter.bitset: " + len + "\n");
+	
+	var len = 0;
+	for (bits in  ip_filter.ranges)
+	{
+		var llen = len;
+		for (key in  ip_filter.ranges[bits])
+			len += 1;
+		
+		WScript.StdErr.Write("ip_filter.ranges[" + bits + "]: " + (len - llen) + "\n");
+	}
+		
+	
+	WScript.StdErr.Write("ip_filter.ranges: " + len + "\n");
+	
 	var ip_list = WScript.StdIn.ReadAll().match(/[^,\s]+/g);
 	var not_found = 0
 	for (var i=0; i < ip_list.length; i++)
